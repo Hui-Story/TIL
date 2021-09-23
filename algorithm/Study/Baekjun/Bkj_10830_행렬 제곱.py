54694ce5a908b70e1927e03957639ae8de61b693
Hui-Story <leejaehee0728@gmail.com>
2021-09-23 20:33:56 +0900

    210923 add TIL

--- algorithm/Study/Baekjun/Bkj_10830_행렬 제곱.py
+++ algorithm/Study/Baekjun/Bkj_10830_행렬 제곱.py
@@ -6,14 +6,17 @@ def power(n):
         return matrix
     elif n == 2:
         return matrix_multiple(matrix, matrix)
+    # 2 로 나누어지는 경우 바로 분할
     elif n % 2 == 0:
         arr = power(n//2)
         return matrix_multiple(arr, arr)
+    # 2 로 나누어 나머지가 생기는 경우 나머지를 다시 곱해줌
     else:
         arr = power(n//2)
         arr2 = matrix_multiple(arr, arr)
         return matrix_multiple(arr2, matrix)
 
+# arr1과 arr2를 곱한 값을 반환 (각 값은 1000으로 나눈 나머지로 표현)
 def matrix_multiple(arr1, arr2):
     result = [[0]*N for _ in range(N)]
     for i in range(N):
@@ -29,6 +32,7 @@ matrix = [list(map(int, input().split())) for _ in range(N)]
 
 result = power(B)
 
+# 행렬곱이 시행되지 않아(B = 1) 나머지로 표현되지 못하는 경우를 위해 나머지로 처리
 for i in range(N):
     for j in range(N):
         result[i][j] = result[i][j] % 1000

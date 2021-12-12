9de597cff7b72d35c5daac53b9a29955ccc603f8
Hui-Story <leejaehee0728@gmail.com>
2021-12-12 22:01:53 +0900

    211212 add TIL

--- algorithm/Study/Baekjun/Bkj_1956_cod.py
+++ algorithm/Study/Baekjun/Bkj_1956_cod.py
@@ -2,4 +2,27 @@ import sys
 input = sys.stdin.readline
 
 V, E = map(int, input().split())
-for _ 
+graph = [[10000000] * V for _ in range(V)]
+for _ in range(E):
+    a, b, c = map(int, input().split())
+    graph[a-1][b-1] = c
+
+for k in range(V):
+    for i in range(V):
+        for j in range(V):
+            dist = graph[i][k] + graph[k][j]
+            if dist < graph[i][j]:
+                graph[i][j] = dist
+
+result = 20000000
+for i in range(V):
+    for j in range(V):
+        if i < j:
+            cycle_dist = graph[i][j] + graph[j][i]
+            if cycle_dist < 10000000:
+                result = min(result, cycle_dist)
+
+if result != 20000000:
+    print(result)
+else:
+    print(-1)

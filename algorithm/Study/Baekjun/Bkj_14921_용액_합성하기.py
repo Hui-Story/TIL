3aa867ef77adeeada0722a0291c592f2df14bc83
Hui-Story <leejaehee0728@gmail.com>
2021-12-20 22:14:00 +0900

    211220 add TIL

---
+++ algorithm/Study/Baekjun/Bkj_14921_용액_합성하기.py
@@ -0,0 +1,18 @@
+N = int(input())
+
+A = list(map(int, input().split()))
+result = 200000000
+
+s, e = 0, N-1
+while s < e:
+    temp = A[s] + A[e]
+    if abs(temp) < abs(result):
+        result = temp
+    if temp < 0:
+        s += 1
+    elif temp > 0:
+        e -= 1
+    else:
+        break
+
+print(result)

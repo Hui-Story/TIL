e82430ca6863bea946544b0d0b73edbf344be3df
Hui-Story <leejaehee0728@gmail.com>
2021-12-22 18:56:11 +0900

    211222 add TIL

---
+++ algorithm/Study/Baekjun/Bkj_5972_택배_배송.py
@@ -0,0 +1,39 @@
+import sys, heapq
+input = sys.stdin.readline
+
+
+def dijkstra():
+    global dist
+    min_heap = []
+    heapq.heappush(min_heap, (dist[1], 1))
+
+    while min_heap:
+        now_dist, now_barn = heapq.heappop(min_heap)
+        if dist[now_barn] < now_dist:
+            continue
+        for next_barn, next_dist in road[now_barn].items():
+            new_dist = now_dist + next_dist
+            if new_dist < dist[next_barn]:
+                dist[next_barn] = new_dist
+                heapq.heappush(min_heap, (new_dist, next_barn))
+
+
+N, M = map(int, input().split())
+
+dist = [float('inf')] * (N+1)
+dist[1] = 0
+road = dict()
+for i in range(1, N+1):
+    road[i] = {}
+
+for _ in range(M):
+    A, B, C = map(int, input().split())
+    if road[A].get(B) and road[A][B] <= C:
+        continue
+    else:
+        road[A][B] = C
+        road[B][A] = C
+
+dijkstra()
+
+print(dist[N])

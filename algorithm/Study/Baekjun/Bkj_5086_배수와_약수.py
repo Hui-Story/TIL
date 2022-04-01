1f51d9f56be5823dc4174a51a194ac38b8468fe1
Hui-Story <leejaehee0728@gmail.com>
2022-04-01 19:50:14 +0900

    220401 add TIL

---
+++ algorithm/Study/Baekjun/Bkj_5086_배수와_약수.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+while True:
+    a, b = map(int, input().split())
+    if a == 0 and b == 0:
+        break
+    if not b % a:
+        print('factor')
+    elif not a % b:
+        print('multiple')
+    else:
+        print('neither')

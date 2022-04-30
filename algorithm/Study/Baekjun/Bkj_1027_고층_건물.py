c011d964d6e46111b5cb37570189311ca843810e
Hui-Story <leejaehee0728@gmail.com>
2022-04-30 17:48:18 +0900

    feat: update Bkj_1027
    '백준 1027 고층 건물' 문제 해결

--- algorithm/Study/Baekjun/Bkj_1027_고층_건물.py
+++ algorithm/Study/Baekjun/Bkj_1027_고층_건물.py
@@ -5,13 +5,12 @@ view_cnt = [0] * N
 for l in range(N - 1):
     for r in range(l + 1, N):
         for n in range(l + 1, r):
-            limit = (buildings[r] - buildings[l]) * ((n - l) / (r - l)) + buildings[l]
-            print(limit)
-            if buildings[n] >= limit:
+            min_slope = (buildings[r] - buildings[l]) / (r - l)
+            now_slope = (buildings[n] - buildings[l]) / (n - l)
+            if now_slope >= min_slope:
                 break
         else:
             view_cnt[l] += 1
             view_cnt[r] += 1
 
-print(*view_cnt)
 print(max(view_cnt))

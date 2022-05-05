6ee62c5f95a472def7ef168489100b0b7cebe032
Hui-Story <leejaehee0728@gmail.com>
2022-05-05 19:02:54 +0900

    feat: add Bkj_1516
    '백준 1516 게임 개발' 문제 해결

--- algorithm/Study/Baekjun/Bkj_1516_게임_개발.py
+++ algorithm/Study/Baekjun/Bkj_1516_게임_개발.py
@@ -1,15 +1,18 @@
 import sys
 input = sys.stdin.readline
 
-def dfs(n, cnt):
+def dfs(n):
     global result
-    result[n] = max(result[n], cnt + times[n])
     for i in pre_lst[n]:
-        dfs(i, result[n])
+        pre_cnt[i] -= 1
+        result[i] = max(result[i], result[n] + times[i])
+        if not pre_cnt[i]:
+            dfs(i)
 
 N = int(input())
 times = [0]
 pre_lst = [[] for _ in range(N + 1)]
+pre_cnt = [0] * (N + 1)
 for i in range(1, N + 1):
     info = list(map(int, input().split()))
     times.append(info[0])
@@ -19,10 +22,11 @@ for i in range(1, N + 1):
     for j in info[1:]:
         if j != -1:
             pre_lst[j].append(i)
+            pre_cnt[i] += 1
 result = times[:]
 
 for n in pre_lst[0]:
-    dfs(n, 0)
+    dfs(n)
 
 for i in result[1:]:
     print(i)

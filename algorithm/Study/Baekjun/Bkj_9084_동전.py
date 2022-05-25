0164d3657013772993aec89b1134afe270131f3c
Hui-Story <leejaehee0728@gmail.com>
2022-05-25 20:53:47 +0900

    feat: solve Bkj_9084
    '백준 9084 동전' 문제 해결

--- algorithm/Study/Baekjun/Bkj_9084_동전.py
+++ algorithm/Study/Baekjun/Bkj_9084_동전.py
@@ -8,4 +8,10 @@ for _ in range(T):
     coins = list(map(int, input().split()))
     M = int(input())
 
+    dp = [1] + [0] * M
+
+    for coin in coins:
+        for i in range(coin, M + 1):
+            dp[i] += dp[i - coin]
     
+    print(dp[-1])

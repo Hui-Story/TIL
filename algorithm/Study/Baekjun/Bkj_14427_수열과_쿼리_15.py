5a35b3429d787df37582bad1625e0f6ec15d98b1
unknown <leejaehee0728@gmail.com>
2022-06-27 23:47:38 +0900

    feat: add Bkj_14427

---
+++ algorithm/Study/Baekjun/Bkj_14427_수열과_쿼리_15.py
@@ -0,0 +1,46 @@
+import sys, math
+from typing import List
+
+input = sys.stdin.readline
+MIIS = lambda: map(int, input().split())
+
+class SegmentTree:
+    def __init__(self, N: int, A: List[int]) -> None:
+        self.N = N
+        self.A = A
+        h = math.ceil(math.log2(N))
+        tree_size = 1 << (h + 1)
+        self.tree = [0] * tree_size
+        self.node_index = [0] * (N + 1)
+        self.find_node_index(1, 1, N)
+    
+    def find_node_index(self, node: int, start: int, end: int) -> None:
+        if start == end:
+            self.node_index[start] = node
+            self.tree[node] = start
+            self.update(node // 2)
+        else:
+            mid = (start + end) // 2
+            self.find_node_index(node * 2, start, mid)
+            self.find_node_index(node * 2 + 1, mid + 1, end)
+    
+    def update(self, i: int, node: int) -> None:
+        while node:
+            self.tree[node] = min(self.tree[node * 2], self.tree[node * 2 + 1])
+            node //= 2
+
+    
+N = int(input())
+A = list(MIIS())
+segment_tree = SegmentTree(N, A)
+M = int(input())
+
+for _ in range(M):
+    query = list(MIIS())
+    if query[0] == 1:
+        i, v = query[1:]
+        segment_tree.A[i - 1] = v
+        segment_tree.tree[segment_tree.node_index[i]] = i
+        segment_tree.update(i, segment_tree.node_index[i] // 2)
+    else:
+        print(segment_tree.tree[0])

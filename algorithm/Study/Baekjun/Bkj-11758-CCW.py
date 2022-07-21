741116cd3939ebd11db29c87ac0a90b1c8efda80
Hui-Story <leejaehee0728@gmail.com>
2022-07-21 16:12:40 +0900

    feat: solve Bkj-11758
    '백준 11758 CCW' 문제 해결

---
+++ algorithm/Study/Baekjun/Bkj-11758-CCW.py
@@ -0,0 +1,22 @@
+import sys
+from typing import List, Tuple
+
+input = sys.stdin.readline
+
+# (x2 - x1)(y3 - y1) - (x3 - x1)(y2 - y1) = (x1y2 + x2y3 + x3y1) - (x2y1 + x3y2 + x1y3)
+def ccw(p1, p2, p3):
+    x1, y1 = p1
+    x2, y2 = p2
+    x3, y3 = p3
+    return (x1 * y2 + x2 * y3 + x3 * y1) - (x2 * y1 + x3 * y2 + x1 * y3)
+
+points: List[Tuple[int]] = [tuple(map(int, input().split())) for _ in range(3)]
+
+answer = ccw(*points)
+
+if answer > 0:
+    print(1)
+elif answer < 0:
+    print(-1)
+else:
+    print(0)

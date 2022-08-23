07dbdf2984c5b39ad76b10e05b2dd77e1be807e3
unknown <leejaehee0728@gmail.com>
2022-08-23 21:46:05 +0900

    feat: solve Bkj-10255
    '백준 10255 교차점' 문제 해결

--- algorithm/Study/Baekjun/Bkj-10255-교차점.py
+++ algorithm/Study/Baekjun/Bkj-10255-교차점.py
@@ -11,12 +11,13 @@ def ccw(p1: Point, p2: Point, p3: Point) -> int:
     return (x2 - x1) * (y3 - y1) - (x3 - x1) * (y2 - y1)
 
 def is_intersection(p1: Point, p2: Point, plt: Point, plb: Point, prt: Point, prb: Point) -> bool:
+    cnt: int = 0
     # 직사각형의 모든 변을 체크
     for p3, p4 in ((plt, plb), (plt, prt), (prt, prb), (plb, prb)):
         ccw1, ccw2, ccw3, ccw4 = ccw(p1, p2, p3), ccw(p1, p2, p4), ccw(p3, p4, p1), ccw(p3, p4, p2)
         if ccw1 * ccw2 <= 0 and ccw3 * ccw4 <= 0:
             # 일직선 상에 있는 경우 포개진 상태인지 확인
-            if ccw1 * ccw2 == 0 and ccw3 * ccw4 == 0:
+            if (p1[0] == p2[0] == p3[0] == p4[0]) or (p1[1] == p2[1] == p3[1] == p4[1]):
                 if (max(p1[0], p2[0]) >= min(p3[0], p4[0]) and max(p3[0], p4[0]) >= min(p1[0], p2[0]))\
                     and (max(p1[1], p2[1]) >= min(p3[1], p4[1]) and max(p3[1], p4[1]) >= min(p1[1], p2[1])):
                     if (max(p1[0], p2[0]) == min(p3[0], p4[0]) or max(p3[0], p4[0]) == min(p1[0], p2[0]))\
@@ -25,8 +26,21 @@ def is_intersection(p1: Point, p2: Point, plt: Point, plb: Point, prt: Point, pr
                     else:
                         return 4
             else:
-                return 5
-    return 0
+                cnt += 1
+    for p in (plt, plb, prt, prb):
+        if p in (p1, p2):
+            cnt -= 1
+            continue
+        if ((min(p1[0], p2[0]) <= p[0]) and (max(p1[0], p2[0]) >= p[0])) and ((min(p1[1], p2[1]) <= p[1]) and (max(p1[1], p2[1]) >= p[1])):
+            if (p[1] - p1[1]) != 0 and (p2[1] - p1[1]) != 0:
+                slope1 = (p[0] - p1[0]) / (p[1] - p1[1])
+                slope2 = (p2[0] - p1[0]) / (p2[1] - p1[1])
+                if slope1 == slope2:
+                    cnt -= 1
+    if cnt >= 2:
+        return 2
+    else:
+        return cnt
 
 
 T: int = int(input())
@@ -62,13 +76,4 @@ for _ in range(T):
             print(2)
         continue
 
-    # 직사각형 경계에 점 1개 위치
-    if ((xl <= x1 <= xr) and (yb <= y1 <= yt)) or ((xl <= x2 <= xr) and (yb <= y2 <= yt)):
-        # 직선이 평행한 경우
-        if (x1 == x2) or (y1 == y2):
-            print(is_intersection((x1, y1), (x2, y2), (xl, yt), (xl, yb), (xr, yt), (xr, yb)))
-        else:
-            print(1)
-        continue
-
-    print(2)
+    print(is_intersection((x1, y1), (x2, y2), (xl, yt), (xl, yb), (xr, yt), (xr, yb)))

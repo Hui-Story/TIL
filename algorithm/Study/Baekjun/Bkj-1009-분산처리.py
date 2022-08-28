73353bcec706c7d04de339d155e3c3bf16fccefa
unknown <leejaehee0728@gmail.com>
2022-08-29 00:30:48 +0900

    feat: solve Bkj-1009
    '백준 1009 분산처리' 문제 해결

---
+++ algorithm/Study/Baekjun/Bkj-1009-분산처리.py
@@ -0,0 +1,24 @@
+import sys
+
+input = sys.stdin.readline
+
+T = int(input())
+
+for _ in range(T):
+    a, b = map(int, input().split())
+    a %= 10
+
+    if a == 0:
+        print(10)
+    elif a in [1, 5, 6]:
+        print(a)
+    elif a in [4, 9]:
+        if b % 2 == 0:
+            print((a * a) % 10)
+        else:
+            print(a % 10)
+    else:
+        if b % 4 == 0:
+            print((a ** 4) % 10)
+        else:
+            print((a ** b) % 10)
